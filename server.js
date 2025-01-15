cbdcfb4a42dc6235ad91c497237e6c42f8600fa9
Teirza Tippets <[Teirza-Tippets]@users.noreply>
2025-01-15 15:30:15 -0700

    added a new route to my practice code

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send(`Hello, ${name}!`); // <-- UPDATED
 });
 
+app.get('/home', (req, res) => {
+    res.send('Welcome, this is the home page!')
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
