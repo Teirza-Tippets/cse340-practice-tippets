aa376506e7fe93eae6a39dec744aacbf90287e75
Teirza Tippets <[Teirza-Tippets]@users.noreply>
2025-01-15 16:16:37 -0700

    prove assignment introducing routing

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 import express from 'express';
+// Add these imports to your existing imports
+import { fileURLToPath } from 'url';
+import  path from 'path';
+
 
 const app = express();
+// Create __dirname and __filename variables
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const name = process.env.NAME; // <-- NEW
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`); // <-- UPDATED
+    res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
-app.get('/home', (req, res) => {
-    res.send('Welcome, this is the home page!')
+app.get('/page1', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/page1.html'));
+});
+
+app.get('/page2', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/page2.html'));
 });
 
 const PORT = 3000;
